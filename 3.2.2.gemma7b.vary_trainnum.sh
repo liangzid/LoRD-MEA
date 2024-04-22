5d3bcac65d0ade2839b3829d04752a1807d47f52
liangzid <2273067585@qq.com>
2024-04-22 21:02:22 +0800

    add 7b our method experiments.

--- 3.2.2.gemma7b.vary_trainnum.sh
+++ 3.2.2.gemma7b.vary_trainnum.sh
@@ -1,35 +1,43 @@
 #!/bin/bash
 ######################################################################
-#3.2.VARY_TRAIN_NUM --- 
+#3.2.2.GEMMA7B.VARY_TRAINNUM ---
+
+# Train with our methods.
 
 # Author: Zi Liang <zi1415926.liang@connect.polyu.hk>
 # Copyright © 2024, ZiLiang, all rights reserved.
-# Created:  5 April 2024
+# Created: 22 April 2024
 ######################################################################
 
 ######################### Commentary ##################################
 ##  
 ######################################################################
 
-
 echo "HOME: ${HOME}"
 export python=${HOME}/anaconda3/envs/align/bin/python3
-export CUDA_VISIBLE_DEVICES="0,1,2,3"
-# export CUDA_VISIBLE_DEVICES="4,5,6,7"
+# export CUDA_VISIBLE_DEVICES="0,1,2,3"
+export CUDA_VISIBLE_DEVICES="4,5,6"
 export root_dir="${HOME}/alignmentExtraction/"
-export POD_save_dir="${root_dir}vArY_TrAiN_nUm_ckpts/"
-export from_path="google/gemma-2b"
+export POD_save_dir="${root_dir}vArY_TrAiN_num_LoRA-LoRD-ckpts/"
+export from_path="google/gemma-7b"
 export msl=256
 export TRAIN_NUMS=("4" "8" "16" "32" "64" "100" "256" "512")
-export TRAIN_NUMS=("32" "64" "100" "256" "512")
-export train_times=("1" "2" "3")
-
+export train_times=(1 2 3)
 # export task_ls=("cola" "mnli" "mrpc" "qnli" "qqp" "rte" "sst2" "wnli")
 # export task_ls=("cs-en" "de-en" "fi-en" "ro-en" "ru-en" "tr-en")
-# export task_ls=("cs-en" "de-en" "fi-en")
 export task_ls=("piqa" "truthful_qa" "allenai/ai2_arc")
-export task_ls=("allenai/ai2_arc")
-export train_taskls=("Complex-lord" "LoRD-II" "LoRD-IV")
+# export task_ls=("allenai/ai2_arc")
+export train_taskls=("LoRD-IV")
+
+# ## ====================TO DEBUG====================
+# export epoch=1
+# export period=2
+# export beta=1.0
+# export temperature=2
+# export batch_size=1
+# ## ====================TO DEBUG====================
+
+export use_lora=1
 
 export epoch=1
 export period=1
@@ -38,8 +46,6 @@ export sub_stage_num=16
 export train_num=100
 export max_new_tokens=64
 
-# export epoch=3
-# export period=3
 export beta=1.0
 export temperature=2
 export batch_size=1
@@ -52,16 +58,15 @@ export use_entropy=0
 # export train_num=100
 export max_new_tokens=64
 
-
 echo "To run this script, you should decide your LoRD method: which one is best? should you use Complex-lord, lord, or reinforce-lord?"
 
-for task in ${task_ls[*]}
+for train_num in ${TRAIN_NUMS[*]}
 do
-    for train_task in ${train_taskls[*]}
+    for train_time in ${train_times[*]}
     do
-	for train_num in ${TRAIN_NUMS[*]}
+	for task in ${task_ls[*]}
 	do
-	    for train_time in ${train_times[*]}
+	    for train_task in ${train_taskls[*]}
 	    do
 		echo "====================================================="
 		echo "+++++++train_num: ${train_num}+++++++"
@@ -73,6 +78,9 @@ do
 		export save_path="${POD_save_dir}varyTrainNum___${train_num}${train_time}${task}${train_task}${epoch}${period}${temperature}${batch_size}${max_new_tokens}${msl}"
 
 		$python lord_train.py\
+		    --use_lora=$use_lora \
+		    --from_path=$from_path \
+		    --task=$train_task \
 		    --device="cuda" \
 		    --epoch=$epoch \
 		    --period_num=$period \
@@ -85,13 +93,11 @@ do
 		    --beta=$beta \
 		    --temperature=$temperature \
 		    --batch_size=$batch_size \
-		    --task=$train_task \
 		    --use_old_logits=$use_old_logits\
 		    --use_vic_logits=$use_vic_logits\
 		    --use_kld=$use_kld\
 		    --max_length=$msl \
 		    --dataset_task=$task \
-		    --from_path=$from_path \
 		    --save_path=$save_path
 		echo "DONE FOR ONE TRAIN NUMBERS...."
 	    done
@@ -102,8 +108,5 @@ done
 
 
 
-
-
-
-echo "RUNNING 3.2.vary_train_num.sh DONE."
-# 3.2.vary_train_num.sh ends here
+echo "RUNNING 3.2.2.gemma7b.vary_trainnum.sh DONE."
+# 3.2.2.gemma7b.vary_trainnum.sh ends here
