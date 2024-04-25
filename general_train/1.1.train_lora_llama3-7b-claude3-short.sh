eefb2c9df48ea0c87922c27c937a0a69c91b2f82
liangzid <2273067585@qq.com>
2024-04-25 13:11:48 +0000

    general training: code of running scripts and data process.

---
+++ general_train/1.1.train_lora_llama3-7b-claude3-short.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+######################################################################
+#1.1.TRAIN_LORA_LLAMA3-7B-CLAUDE3-SHORT --- 
+
+# Author: Zi Liang <zi1415926.liang@connect.polyu.hk>
+# Copyright © 2024, ZiLiang, all rights reserved.
+# Created: 25 April 2024
+######################################################################
+
+######################### Commentary ##################################
+##  
+######################################################################
+
+echo "HOME: ${HOME}"
+export python=${HOME}/anaconda3/envs/align/bin/python3
+# export CUDA_VISIBLE_DEVICES="0,1,2,3"
+export CUDA_VISIBLE_DEVICES="4,5,6"
+export root_dir="${HOME}/alignmentExtraction/"
+export POD_save_dir="${root_dir}/general_train/ckpts/boring_test/"
+export from_path="meta-llama/Meta-Llama-3-8B-Instruct"
+export msl=1024
+export TRAIN_NUMS=(2)
+export train_times=(1)
+export task_ls=("liangzid/claude3_short256")
+# export train_taskls=("vanilla")
+export train_taskls=("LoRD-II")
+# export train_taskls=("LoRD-IV")
+
+# ## ====================TO DEBUG====================
+# export epoch=1
+# export period=2
+# export beta=1.0
+# export temperature=2
+# export batch_size=1
+# ## ====================TO DEBUG====================
+
+export use_lora=1
+
+export epoch=1
+export period=1
+export sub_set_num=8
+export sub_stage_num=16
+export train_num=100
+export max_new_tokens=64
+
+export beta=1.0
+export temperature=2
+export batch_size=1
+
+export use_old_logits=1
+export use_vic_logits=1
+export use_kld=0
+export use_entropy=0
+
+# export train_num=100
+export max_new_tokens=64
+
+for train_num in ${TRAIN_NUMS[*]}
+do
+    for train_time in ${train_times[*]}
+    do
+	for task in ${task_ls[*]}
+	do
+	    for train_task in ${train_taskls[*]}
+	    do
+		echo "====================================================="
+		echo "+++++++train_num: ${train_num}+++++++"
+		echo "+++++++train_time: ${train_time}+++++++"
+		echo "+++++++task: ${task}+++++++"
+		echo "+++++++train_task: ${train_task}+++++++"
+		echo "====================================================="
+
+		export save_path="${POD_save_dir}BORING_TEST___${train_num}${train_time}${task}${train_task}${epoch}${period}${temperature}${batch_size}${max_new_tokens}${msl}"
+
+		$python ${root_dir}lord_train.py\
+		    --use_lora=$use_lora \
+		    --from_path=$from_path \
+		    --task=$train_task \
+		    --device="cuda" \
+		    --epoch=$epoch \
+		    --period_num=$period \
+		    --acc_step=1 \
+		    --log_step=50 \
+		    --save_step=100000 \
+		    --train_num=$train_num \
+		    --max_new_tokens=$max_new_tokens \
+		    --LR="3e-5" \
+		    --beta=$beta \
+		    --temperature=$temperature \
+		    --batch_size=$batch_size \
+		    --use_old_logits=$use_old_logits\
+		    --use_vic_logits=$use_vic_logits\
+		    --use_kld=$use_kld\
+		    --max_length=$msl \
+		    --dataset_task=$task \
+		    --save_path=$save_path
+		echo "DONE FOR ONE TRAIN NUMBERS...."
+	    done
+	done
+    done
+done
+
+
+
+
+
+
+
+
+
+
+
+echo "RUNNING 1.1.train_lora_llama3-7b-claude3-short.sh DONE."
+# 1.1.train_lora_llama3-7b-claude3-short.sh ends here
---
+++ general_train/general_preprocess.py
@@ -0,0 +1,159 @@
+"""
+======================================================================
+GENERAL_TRAIN --- general pretraining with vanilla method and our lora
+methods.
+
+    Author: Zi Liang <zi1415926.liang@connect.polyu.hk>
+    Copyright © 2024, ZiLiang, all rights reserved.
+    Created: 25 April 2024
+======================================================================
+"""
+
+
+# ------------------------ Code --------------------------------------
+
+## normal import 
+import json
+from typing import List,Tuple,Dict
+import random
+from pprint import pprint as ppp
+# import pickle
+# import os
+# from os.path import join, exists
+# from collections import Counter,OrderedDict
+# from bisect import bisect
+# from copy import deepcopy
+# import pickle
+
+# ## transformers related import
+# from transformers import T5Tokenizer,T5ForConditionalGeneration
+# from transformers import BertTokenizer
+# from transformers import pipeline
+# import transformers
+import os
+from collections import OrderedDict
+
+from datasets import load_dataset
+from tqdm import tqdm
+
+
+def ___data_cleaning():
+    dirp="./datas"
+    list_files=os.listdir(dirp)
+    fpths=[dirp+"/"+x for x in list_files]
+
+    datalist=[]
+    for fname in fpths:
+        # from collections import OrderedDict
+        with open(fname, 'r',encoding='utf8') as f:
+            data=json.load(f,object_pairs_hook=OrderedDict)
+        datalist.extend(data)
+
+    print(len(datalist))
+
+    # with open("./datas/overall_cleaned_data.json",
+    #           'w',encoding='utf8') as f:
+    #     json.dump(datalist, f, ensure_ascii=False, indent=4)
+
+    # print("Save done.")
+
+    ## transfer to huggingface formation.
+
+    # resls=[]
+    # for x in datalist:
+    #     resls.append(json.dumps(x, ensure_ascii=False,))
+    # restext="\n".join(resls)
+    
+    # with open("./datas/hf_overall_cleaned_data.jsonl",
+    #           'w',encoding='utf8') as f:
+    #     f.write(restext)
+    # print("Save done.")
+
+    ## filter a short subset with length < N
+    N=256
+    newresls=[]
+    for x in datalist:
+        num_inps=len(x["prompt"].split(" "))
+        num_out=len(x["response"].split(" "))
+        if num_inps+num_out<=N:
+            newresls.append(x)
+
+    print(f"Data Num After filtered: {len(newresls)}")
+    ## save this short texts.
+
+    resls=[]
+    for x in newresls:
+        resls.append(json.dumps(x, ensure_ascii=False,))
+    restext="\n".join(resls)
+    
+    with open("./datas/hf_overall_cleaned_data_short256_249.jsonl",
+              'w',encoding='utf8') as f:
+        f.write(restext)
+    print("Save done.")
+
+
+def loading_data(
+        dataset_name="liangzid/claude3_chat3.3k"):
+
+    dataset=load_dataset(dataset_name, split="train")
+    inpsls=[]
+    outls=[]
+
+    print(dataset)
+
+    for item in tqdm(dataset):
+        print(f"ITEM: {item}")
+        inp = item["prompt"]
+        out = item["response"]
+        inpsls.append(inp)
+        outls.append(out)
+    print(f"total length: {len(inpsls)}")
+    return inpsls,outls
+
+def data_format_transform(inpsls,outls,tokenizer,
+                          train_num,
+                          max_length,
+                          topk=1,):
+    p_idxls = []
+    text2ls = []
+
+    for iii_bgn, q in tqdm(enumerate(inpsls),
+                            desc="..."):
+        if iii_bgn==train_num:
+            break
+        p_idxls.append(tokenizer(q,
+                                 max_length=max_length,
+                                 return_tensors="pt").input_ids[0])
+
+        text=f"User: {q} Assistant: {outls[iii_bgn]}"
+        text2ls.append(tokenizer(text,
+                                 max_length=max_length,
+                                 return_tensors="pt").input_ids[0])
+
+    return p_idxls, text2ls, None, None
+
+
+def general_load_data(
+    tokenizer,
+        train_num=2,
+        max_length=1024,
+    topk=1,
+    dataset_name="liangzid/claude3_chat3.3k",
+        ):
+    ips,ots=loading_data(dataset_name)
+    return data_format_transform(ips,ots,tokenizer,
+                                 train_num,max_length,
+                                 topk=topk)
+
+## running entry
+if __name__=="__main__":
+    from transformers import AutoTokenizer
+    tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b")
+    res=general_load_data(tokenizer,
+                          dataset_name="liangzid/claude3_short256")
+    print(res)
+
+    # ___data_cleaning()
+
+
+    # print("EVERYTHING DONE.")
