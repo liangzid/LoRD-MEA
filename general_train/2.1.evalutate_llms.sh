5f2e8a5d1f1b161459fc5dee84912340ba28b3d5
liangzid <2273067585@qq.com>
2024-04-26 02:56:49 +0000

    add the EAI'harness based generalized evaluation.

---
+++ general_train/2.1.evalutate_llms.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+######################################################################
+#2.1.EVALUTATE_LLMS ---
+
+# Evaluate LLMs with the EAI's harness
+
+# Author: Zi Liang <zi1415926.liang@connect.polyu.hk>
+# Copyright © 2024, ZiLiang, all rights reserved.
+# Created: 26 April 2024
+######################################################################
+
+######################### Commentary ##################################
+##  
+######################################################################
+
+echo "HOME: ${HOME}"
+export python=${HOME}/anaconda3/envs/align/bin/python3
+# export CUDA_VISIBLE_DEVICES="0,1,2,3"
+# export CUDA_VISIBLE_DEVICES="4,5,6,7"
+export CUDA_VISIBLE_DEVICES="4,5"
+# export CUDA_VISIBLE_DEVICES="6,7"
+export root_dir="${HOME}/alignmentExtraction/"
+export save_dir="${root_dir}/general_train/ckpts/boring_test/"
+export from_path="meta-llama/Meta-Llama-3-8B-Instruct"
+
+# export task_ls=("liangzid/claude3_short256")
+# export train_taskls=("LoRD-II" "LoRD-IV")
+export m="LoRD-II"
+# export m="LoRD-IV"
+export pmp=meta-llama/Meta-Llama-3-8B-Instruct
+export fmp="${save_dir}BORING_TEST___2491liangzid/claude3_short256${m}1121641024___period8/"
+
+# export qas=openbookqa,arc_easy,winogrande,hellaswag,arc_challenge,piqa,boolq
+export qas=piqa
+
+export eval=${HOME}/anaconda3/envs/align/bin/lm_eval
+
+echo "================================================================"
+echo "EVALUATION MODEL: pretrained: ${pmp} lora: ${fmp}"
+echo "EVALUATION TASKS: ${qas}"
+echo "================================================================"
+
+$eval --model hf \
+    --model_args pretrained=${pmp},parallelize=True,peft=${fmp}\
+    --tasks $qas\
+    --device cuda\
+    --batch_size auto:4
+
+echo "RUNNING 2.1.evalutate_llms.sh DONE."
+# 2.1.evalutate_llms.sh ends here
