5621a750e2763d056735cbe19eda1aaa37a2217e
liangzid <2273067585@qq.com>
2024-04-28 02:20:53 +0000

    backup new records.

--- general_train/2.1.evalutate_llms.sh
+++ general_train/2.1.evalutate_llms.sh
@@ -18,7 +18,8 @@ export python=${HOME}/anaconda3/envs/align/bin/python3
 # export CUDA_VISIBLE_DEVICES="0,1,2,3"
 # export CUDA_VISIBLE_DEVICES="4,5,6,7"
 # export CUDA_VISIBLE_DEVICES="4,5"
-export CUDA_VISIBLE_DEVICES="6,7"
+# export CUDA_VISIBLE_DEVICES="6,7"
+export CUDA_VISIBLE_DEVICES="0,1"
 export root_dir="${HOME}/alignmentExtraction/"
 export save_dir="${root_dir}/general_train/ckpts/boring_test/"
 export from_path="meta-llama/Meta-Llama-3-8B-Instruct"
@@ -29,28 +30,14 @@ export qas=piqa
 export eval=${HOME}/anaconda3/envs/align/bin/lm_eval
 export pmp=meta-llama/Meta-Llama-3-8B-Instruct
 
-# # export task_ls=("liangzid/claude3_short256")
-# # export train_taskls=("LoRD-II" "LoRD-IV")
-# export m="LoRD-II"
-# # export m="LoRD-IV"
-# export fmp="${save_dir}BORING_TEST___2491liangzid/claude3_short256${m}1121641024___period8/"
+## LoRD-II Inference.
+# export task_ls=("liangzid/claude3_short256")
+# export train_taskls=("LoRD-II" "LoRD-IV")
+export m="LoRD-II"
+# export m="LoRD-IV"
+export fmp="${save_dir}longtext2491liangzid/claude3_short256LoRD-II2121256256___period2/"
 
-
-# echo "================================================================"
-# echo "EVALUATION MODEL: pretrained: ${pmp} lora: ${fmp}"
-# echo "EVALUATION TASKS: ${qas}"
-# echo "================================================================"
-
-# $eval --model hf \
-#     --model_args pretrained=${pmp},parallelize=True,peft=${fmp}\
-#     --tasks $qas\
-#     --device cuda\
-#     --batch_size auto
-
-
-## Vanilla LoRA TRAIN on LLAMA3 8B
-export fmp="${save_dir}BORING_TEST___2491liangzid/claude3_short256vanilla3321641024___finally/"
-export fmp=${save_dir}longtext2491liangzid/claude3_short256vanilla3321256256___finally
+# export fmp="${save_dir}longtext2491liangzid/claude3_short256Complex-lord1521256256___period2"
 
 echo "================================================================"
 echo "EVALUATION MODEL: pretrained: ${pmp} lora: ${fmp}"
@@ -62,6 +49,22 @@ $eval --model hf \
     --tasks $qas\
     --device cuda\
     --batch_size auto
+
+
+# ## Vanilla LoRA TRAIN on LLAMA3 8B
+# export fmp="${save_dir}BORING_TEST___2491liangzid/claude3_short256vanilla3321641024___finally/"
+# export fmp=${save_dir}longtext2491liangzid/claude3_short256vanilla3321256256___finally
+
+# echo "================================================================"
+# echo "EVALUATION MODEL: pretrained: ${pmp} lora: ${fmp}"
+# echo "EVALUATION TASKS: ${qas}"
+# echo "================================================================"
+
+# $eval --model hf \
+#     --model_args pretrained=${pmp},parallelize=True,peft=${fmp}\
+#     --tasks $qas\
+#     --device cuda\
+#     --batch_size auto
 
 
 # # ORIGINAL LLAMA3 8B EVAL
