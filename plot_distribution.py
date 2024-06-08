616eb2c8b89ef516db370751f9b94de590132979
liangzid <2273067585@qq.com>
2024-06-08 17:59:07 +0800

    add the visualization code.

--- plot_distribution.py
+++ plot_distribution.py
@@ -10,6 +10,10 @@ Plot the distribution of generation among different checkpoints.
 ======================================================================
 """
 
+import os
+if __name__=="__main__":
+    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+    os.environ["TORCH_USE_CUDA_DSA"]="1"
 
 # ------------------------ Code --------------------------------------
 
@@ -34,12 +38,16 @@ import numpy as np
 from matplotlib import pyplot as plt
 from collections import OrderedDict
 
+from peft import PeftModel
+
 
 def get_dist_mat(ckpt_pth, task_name,
+                 pretrained_model=None,
                  select_num=3,
                  train_num=100, max_length=256,
                  max_new_tokens=3,
                  only_original=False,
+                 dataset_name="text2sql",
                  ):
 
     # model = InferObj(model_name=ckpt_pth,
@@ -50,25 +58,55 @@ def get_dist_mat(ckpt_pth, task_name,
     if task_name in task_prompt_map:
         prompt = task_prompt_map
 
-    lm = AutoModelForCausalLM.from_pretrained(
-        ckpt_pth,
-        device_map="auto",
-    )
-
-    lm_tokenizer = AutoTokenizer.from_pretrained(ckpt_pth)
-    tokenizer = AutoTokenizer.from_pretrained(ckpt_pth)
+    if pretrained_model is None:
+        lm = AutoModelForCausalLM.from_pretrained(
+            ckpt_pth,
+            device_map="auto",
+            torch_dtype=torch.bfloat16,
+        )
+        lm_tokenizer = AutoTokenizer.from_pretrained(ckpt_pth)
+        tokenizer = AutoTokenizer.from_pretrained(ckpt_pth)
+    else:
+        base_model_name=pretrained_model 
+        lm = AutoModelForCausalLM.from_pretrained(
+            base_model_name,
+            device_map="auto",
+            # trust_remote_code=True,
+            torch_dtype=torch.bfloat16,
+        )
+        lm = PeftModel.from_pretrained(lm, ckpt_pth,)
+        lm_tokenizer = AutoTokenizer.from_pretrained(base_model_name)
+        tokenizer = AutoTokenizer.from_pretrained(base_model_name)
 
     # if lm_tokenizer.pad_token is None:
     lm_tokenizer.pad_token = lm_tokenizer.eos_token
     tokenizer.pad_token = tokenizer.eos_token
 
-    raw_train_datals = load_glue_datals(
-        tokenizer,
-        task_name=task_name,
-        train_num=train_num,
-        max_length=max_length)
+    if dataset_name=="glue":
+        raw_train_datals = load_glue_datals(
+            tokenizer,
+            task_name=task_name,
+            train_num=train_num,
+            max_length=max_length)
 
-    p_ls, idx2ls, logits2ls, idx2_distls = raw_train_datals
+        p_ls, idx2ls, logits2ls, idx2_distls = raw_train_datals
+
+    elif dataset_name=="qa":
+        from qa_process import load_qa_datals
+        raw_train_datals = load_qa_datals(
+            tokenizer,
+            task_name=task_name,
+            train_num=train_num,
+            max_length=max_length)
+        p_ls, idx2ls, logits2ls, idx2_distls = raw_train_datals
+    elif dataset_name=="text2sql":
+        from text2sql_process import load_text2sql_datals
+        raw_train_datals = load_text2sql_datals(
+            tokenizer,
+            task_name=task_name,
+            train_num=train_num,
+            max_length=max_length)
+        p_ls, idx2ls, logits2ls, idx2_distls = raw_train_datals
 
     matrix_ls = []
     for i in range(select_num):
@@ -113,54 +151,72 @@ def get_dist_mat(ckpt_pth, task_name,
 
 
 def visualize_heat(
-        lord_ckpt="./GLUE_ckpts/colaComplex-lord256100___period2/",
-        ce_ckpt="./GLUE_ckpts/colavanilla256100___finally/",
-        kd_ckpt="./POD_SAVE_CKPTs/vary_period0306cs-en/kd_256cs-en_newkd___finally/",
+        # lord_ckpt="./GLUE_ckpts/colaComplex-lord256100___period2/",
+        # ce_ckpt="./GLUE_ckpts/colavanilla256100___finally/",
+        # kd_ckpt="./POD_SAVE_CKPTs/vary_period0306cs-en/kd_256cs-en_newkd___finally/",
+
+        lord_ckpt="./text2sql_ckpts/text2sqlspider161vanilla___finally/",
+        ce_ckpt="./text2sql_ckpts/text2sqlspider161LoRD-VI___period256/",
+        kd_ckpt=None,
+        pretrained_model_pth="meta-llama/Meta-Llama-3-8B-Instruct",
         select_num=8,
+        train_num=16,
+        task_name="spider",
         save_path="distribute_heat_res.pdf",):
 
     origin_mat = get_dist_mat(ckpt_pth=lord_ckpt,
-                              task_name="cola",
+                              pretrained_model=pretrained_model_pth,
+                              task_name=task_name,
                               select_num=select_num,
-                              train_num=100,
+                              train_num=train_num,
                               only_original=True,
                               )
     lord_mat = get_dist_mat(ckpt_pth=lord_ckpt,
-                            task_name="cola",
+                            pretrained_model=pretrained_model_pth,
+                            task_name=task_name,
                             select_num=select_num,
-                            train_num=100,
+                            train_num=train_num,
                             only_original=False,
                             )
     ce_mat = get_dist_mat(ckpt_pth=ce_ckpt,
-                          task_name="cola",
+                          pretrained_model=pretrained_model_pth,
+                          task_name=task_name,
                           select_num=select_num,
-                          train_num=100,
+                          train_num=train_num,
                           only_original=False,
                           )
-    kd_mat = get_dist_mat(ckpt_pth=kd_ckpt,
-                          task_name="cola",
+    init_mat = get_dist_mat(ckpt_pth=pretrained_model_pth,
+                          pretrained_model=None,
+                          task_name=task_name,
                           select_num=select_num,
-                          train_num=100,
+                          train_num=train_num,
                           only_original=False,
                           )
+    # kd_mat = get_dist_mat(ckpt_pth=kd_ckpt,
+    #                       task_name="cola",
+    #                       select_num=select_num,
+    #                       train_num=train_num,
+    #                       only_original=False,
+    #                       )
 
     res_dict = OrderedDict({"Victim model": origin_mat,
+                            "Local Model": init_mat,
                             "LoRD": lord_mat,
                             "Cross-Entropy": ce_mat,
-                            "Distillation": kd_mat,
+                            # "Distillation": kd_mat,
                             })
 
     res_dict = OrderedDict({"Victim model": origin_mat,
+                            "Local Model": init_mat,
                             "LoRD": lord_mat,
                             "Cross-Entropy": ce_mat,
-                            "Distillation": kd_mat,
+                            # "Distillation": kd_mat,
                             })
     with open("./3d_res.pkkl", 'wb') as f:
         pickle.dump(res_dict, f,)
 
     with open("./3d_res.pkkl", 'rb') as f:
         res_dict = pickle.load(f)
-
 
 
     xls = list(res_dict.keys())
@@ -273,4 +329,25 @@ def visualize_3d(
 
 if __name__ == "__main__":
     visualize_heat()
+
+    # visualize_heat(
+    #     lord_ckpt="./text2sql_ckpts/text2sqlspider161vanilla___finally/",
+    #     ce_ckpt="./text2sql_ckpts/text2sqlspider161LoRD-VI___period256/",
+    #     kd_ckpt=None,
+    #     pretrained_model_pth="meta-llama/Meta-Llama-3-8B-Instruct",
+    #     select_num=8,
+    #     train_num=16,
+    #     task_name="spider",
+    #     save_path="distribute_heat_res.pdf",)
+
+    visualize_heat(
+        lord_ckpt="./text2sql_ckpts/text2sqlwikisql161vanilla___finally/",
+        ce_ckpt="./text2sql_ckpts/text2sqlwikisql161LoRD-VI___period256/",
+        kd_ckpt=None,
+        pretrained_model_pth="meta-llama/Meta-Llama-3-8B-Instruct",
+        select_num=8,
+        train_num=16,
+        task_name="wikisql",
+        save_path="distribute_heat_res.pdf",)
+
     # visualize_3d()
