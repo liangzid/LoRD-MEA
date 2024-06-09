dd4d529c39b763bbbeaaa33612f763d4e8213675
liangzid <2273067585@qq.com>
2024-06-09 16:53:13 +0800

    update the code of huggingface's LLM eval.

--- general_train/1.1.train_lora_llama3-7b-claude3-short.sh
+++ general_train/1.1.train_lora_llama3-7b-claude3-short.sh
@@ -94,36 +94,36 @@ do
 		# # export save_path="${POD_save_dir}NewTemperature${train_task}NewLoss"
 		export save_path="${POD_save_dir}NewTemperatureNewTau${train_task}NewLoss"
 
-		$python ${root_dir}lord_train.py\
-		    --use_lora=$use_lora \
-		    --from_path=$from_path \
-		    --is_black_box=$is_black_box \
-		    --sub_set_num=$sub_set_num \
-		    --sub_stage_num=$sub_stage_num\
-		    --infer_batch_size=$infer_batch_size\
-		    --T=$temperature\
-		    --tau1=$tau1 \
-		    --tau2=$tau2 \
-		    --task=$train_task \
-		    --device="cuda" \
-		    --epoch=$epoch \
-		    --period_num=$period \
-		    --acc_step=1 \
-		    --save_step=$save_step \
-		    --log_step=50 \
-		    --train_num=$train_num \
-		    --max_new_tokens=$max_new_tokens \
-		    --LR="3e-5" \
-		    --beta=$beta \
-		    --temperature=$temperature \
-		    --batch_size=$batch_size \
-		    --use_old_logits=$use_old_logits\
-		    --use_vic_logits=$use_vic_logits\
-		    --use_kld=$use_kld\
-		    --max_length=$msl \
-		    --dataset_task=$task \
-		    --save_path=$save_path
-		echo "DONE FOR ONE TRAIN NUMBERS...."
+		# $python ${root_dir}lord_train.py\
+		#     --use_lora=$use_lora \
+		#     --from_path=$from_path \
+		#     --is_black_box=$is_black_box \
+		#     --sub_set_num=$sub_set_num \
+		#     --sub_stage_num=$sub_stage_num\
+		#     --infer_batch_size=$infer_batch_size\
+		#     --T=$temperature\
+		#     --tau1=$tau1 \
+		#     --tau2=$tau2 \
+		#     --task=$train_task \
+		#     --device="cuda" \
+		#     --epoch=$epoch \
+		#     --period_num=$period \
+		#     --acc_step=1 \
+		#     --save_step=$save_step \
+		#     --log_step=50 \
+		#     --train_num=$train_num \
+		#     --max_new_tokens=$max_new_tokens \
+		#     --LR="3e-5" \
+		#     --beta=$beta \
+		#     --temperature=$temperature \
+		#     --batch_size=$batch_size \
+		#     --use_old_logits=$use_old_logits\
+		#     --use_vic_logits=$use_vic_logits\
+		#     --use_kld=$use_kld\
+		#     --max_length=$msl \
+		#     --dataset_task=$task \
+		#     --save_path=$save_path
+		# echo "DONE FOR ONE TRAIN NUMBERS...."
 
 
 # export qas=openbookqa,arc_easy,winogrande,hellaswag,arc_challenge,piqa,boolq
---
+++ general_train/2.2.huggingface_llm_eval.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+######################################################################
+#2.2.HUGGINGFACE_LLM_EVAL ---
+
+# Evaluate the results with huggingface's LLM leaderboard!
+
+# Author: Zi Liang <zi1415926.liang@connect.polyu.hk>
+# Copyright © 2024, ZiLiang, all rights reserved.
+# Created:  9 June 2024
+######################################################################
+
+echo "HOME: ${HOME}"
+export python=${HOME}/anaconda3/envs/align/bin/python3
+export CUDA_VISIBLE_DEVICES="1"
+export root_dir="${HOME}/alignmentExtraction/"
+export POD_save_dir="${root_dir}/general_train/ckpts/boring_test/"
+export from_path="meta-llama/Meta-Llama-3-8B-Instruct"
+# export from_path="Vezora/Mistral-22B-v0.1"
+export pmp=$from_path
+
+
+# export train_taskls=("LoRD-VIII")
+export train_task="LoRD-VIII"
+# # export save_path="${POD_save_dir}NewTemperature${train_task}NewLoss"
+export save_path="${POD_save_dir}NewTemperatureNewTau${train_task}NewLoss"
+
+
+export eval=${HOME}/anaconda3/envs/align/bin/lm_eval
+
+export fmp="${save_path}___period500/"
+
+echo "================================================================"
+echo "EVALUATION MODEL: pretrained: ${pmp} lora: ${fmp}"
+echo "EVALUATION TASKS: ${qas}"
+echo "================================================================"
+
+export evaltasks=arc_challenge
+export fewshot_number=25
+$eval --model hf \
+    --model_args pretrained=${pmp},parallelize=True,peft=${fmp}\
+    --tasks $evaltasks\
+    --num_fewshot=$fewshot_number\
+    --device cuda\
+    --batch_size auto
+
+export evaltasks=hellaswag
+export fewshot_number=10
+$eval --model hf \
+    --model_args pretrained=${pmp},parallelize=True,peft=${fmp}\
+    --tasks $evaltasks\
+    --num_fewshot=$fewshot_number\
+    --device cuda\
+    --batch_size auto
+
+export evaltasks=truthfulqa
+$eval --model hf \
+    --model_args pretrained=${pmp},parallelize=True,peft=${fmp}\
+    --tasks $evaltasks\
+    --device cuda\
+    --batch_size auto
+
+export evaltasks=mmlu
+export fewshot_number=5
+$eval --model hf \
+    --model_args pretrained=${pmp},parallelize=True,peft=${fmp}\
+    --tasks $evaltasks\
+    --num_fewshot=$fewshot_number\
+    --device cuda\
+    --batch_size auto
+
+export evaltasks=winogrande
+export fewshot_number=5
+$eval --model hf \
+    --model_args pretrained=${pmp},parallelize=True,peft=${fmp}\
+    --tasks $evaltasks\
+    --num_fewshot=$fewshot_number\
+    --device cuda\
+    --batch_size auto
+
+export evaltasks=gsm8k
+export fewshot_number=5
+$eval --model hf \
+    --model_args pretrained=${pmp},parallelize=True,peft=${fmp}\
+    --tasks $evaltasks\
+    --num_fewshot=$fewshot_number\
+    --device cuda\
+    --batch_size auto
+
+
+echo "RUNNING 2.2.huggingface_llm_eval.sh DONE."
+# 2.2.huggingface_llm_eval.sh ends here
