57b2aafca33067397a0e14732029d910033eb58f
liangzid <2273067585@qq.com>
2024-06-11 11:22:16 +0800

    watermark inferences.

--- watermark/watermark_detect.py
+++ watermark/watermark_detect.py
@@ -79,6 +79,15 @@ def eval_varying_train_num():
         # "256",
         # "512",
         ]
+    lambda1ls=[
+        "0.0",
+        "0.2",
+        "0.4",
+        "0.6",
+        "0.8",
+        "1.0",
+        ]
+
     base_model_name1="meta-llama/Meta-Llama-3-8B-Instruct"
 
     tokenizer=AutoTokenizer.from_pretrained(base_model_name1)
@@ -97,125 +106,131 @@ def eval_varying_train_num():
         for train_num in train_nums:
             for m in mls:
                 temp_scorels=[]
-                for itime in train_times:
-
-                    if task=="e2e_nlg" or task=="allenai/common_gen":
-                        prefix = "./watermark/d2t_ckpts/D2TTT"
-                        if m=="vanilla":
-                            ckpt = (
-                                prefix
-                                + f"{task}@wrmk{train_num}{itime}{m}___finally/"
-                            )
-                        elif m =="pretrained":
-                            ckpt = f"./text2sql_ckpts/d2t---{task}@wrmk{train_num}{itime}{m}_res.json"
-                        elif m=="gpt-3.5-turbo-1106":
-                            ckpt=m
-                        else:
-                            ckpt = prefix + \
-                                f"{task}@wrmk{train_num}{itime}{m}___period512/"
-                        res_pth = ckpt+f"___{task}@wrmk_d2t_infer_res.json"
-                        res_pth = res_pth.replace("/", "__").replace(".", "")
-
-                        if not os.path.exists(dir_p+res_pth):
-                            if m=="pretrained":
-                                res_ls = infer_d2t(None,
-                                                task,
-                                                dir_p+res_pth,
-                                                test_set_take_num=500,
-                                                mnt=64,
-                                                base_model_name=base_model_name1,
-                                                )
+                for lambda1 in lambda1ls:
+                    for itime in train_times:
+                        if task=="e2e_nlg" or task=="allenai/common_gen":
+                            prefix = "./watermark/d2t_ckpts/D2TTT"
+                            if m=="vanilla":
+                                # ckpt = (
+                                #     prefix
+                                #     + f"{task}@wrmk{train_num}{itime}{m}___finally/"
+                                # )
+                                ckpt = (
+                                    prefix
+                                    + f"{task}@wrmk{train_num}{itime}{m}{lambda1}___finally/"
+                                )
+                            elif m =="pretrained":
+                                ckpt = f"./text2sql_ckpts/d2t---{task}@wrmk{train_num}{itime}{m}_res.json"
+                            elif m=="gpt-3.5-turbo-1106":
+                                ckpt=m
                             else:
-                                res_ls = infer_d2t(ckpt,
-                                                task,
-                                                dir_p+res_pth,
-                                                test_set_take_num=500,
-                                                mnt=64,
-                                                base_model_name=base_model_name1,
-                                                )
-                        else:
-                            # from collections import OrderedDict
-                            with open(dir_p+res_pth, 'r', encoding='utf8') as f:
-                                res_ls = json.load(
-                                    f, object_pairs_hook=OrderedDict)
+                                # ckpt = prefix + \
+                                #     f"{task}@wrmk{train_num}{itime}{m}___period512/"
+                                ckpt = prefix + \
+                                    f"{task}@wrmk{train_num}{itime}{m}{lambda1}___period512/"
+                            res_pth = ckpt+f"___{task}@wrmk_d2t_infer_res.json"
+                            res_pth = res_pth.replace("/", "__").replace(".", "")
 
-                        scores = eval_d2ttt(res_ls)
-                    elif task=="cs-en" or task=="de-en":
-                        prefix = "./watermark/d2t_ckpts/D2TTT"
-                        if m=="vanilla":
-                            ckpt = (
-                                prefix
-                                + f"{task}@wrmk{train_num}{itime}{m}___finally/"
-                            )
-                        elif m =="pretrained":
-                            ckpt = f"./text2sql_ckpts/d2t---{task}@wrmk{train_num}{itime}{m}_res.json"
-                        elif m=="gpt-3.5-turbo-1106":
-                            ckpt=m
-                        else:
-                            ckpt = prefix + \
-                                f"{task}@wrmk{train_num}{itime}{m}___period512/"
-                        res_pth = ckpt+f"___{task}@wrmk_d2t_infer_res.json"
-                        res_pth = res_pth.replace("/", "__").replace(".", "")
-
-                        if not os.path.exists(dir_p+res_pth):
-                            if m=="pretrained":
-                                res_ls = infer_wmt(None,
-                                                task,
-                                                dir_p+res_pth,
-                                                test_set_take_num=500,
-                                                mnt=64,
-                                                base_model_name=base_model_name1,
-                                                )
+                            if not os.path.exists(dir_p+res_pth):
+                                if m=="pretrained":
+                                    res_ls = infer_d2t(None,
+                                                    task,
+                                                    dir_p+res_pth,
+                                                    test_set_take_num=500,
+                                                    mnt=64,
+                                                    base_model_name=base_model_name1,
+                                                    )
+                                else:
+                                    res_ls = infer_d2t(ckpt,
+                                                    task,
+                                                    dir_p+res_pth,
+                                                    test_set_take_num=500,
+                                                    mnt=64,
+                                                    base_model_name=base_model_name1,
+                                                    )
                             else:
-                                res_ls = infer_wmt(ckpt,
-                                                task,
-                                                dir_p+res_pth,
-                                                test_set_take_num=500,
-                                                mnt=64,
-                                                base_model_name=base_model_name1,
-                                                )
+                                # from collections import OrderedDict
+                                with open(dir_p+res_pth, 'r', encoding='utf8') as f:
+                                    res_ls = json.load(
+                                        f, object_pairs_hook=OrderedDict)
+
+                            scores = eval_d2ttt(res_ls)
+                        elif task=="cs-en" or task=="de-en":
+                            prefix = "./watermark/d2t_ckpts/D2TTT"
+                            if m=="vanilla":
+                                ckpt = (
+                                    prefix
+                                    + f"{task}@wrmk{train_num}{itime}{m}___finally/"
+                                )
+                            elif m =="pretrained":
+                                ckpt = f"./text2sql_ckpts/d2t---{task}@wrmk{train_num}{itime}{m}_res.json"
+                            elif m=="gpt-3.5-turbo-1106":
+                                ckpt=m
+                            else:
+                                ckpt = prefix + \
+                                    f"{task}@wrmk{train_num}{itime}{m}___period512/"
+                            res_pth = ckpt+f"___{task}@wrmk_d2t_infer_res.json"
+                            res_pth = res_pth.replace("/", "__").replace(".", "")
+
+                            if not os.path.exists(dir_p+res_pth):
+                                if m=="pretrained":
+                                    res_ls = infer_wmt(None,
+                                                    task,
+                                                    dir_p+res_pth,
+                                                    test_set_take_num=500,
+                                                    mnt=64,
+                                                    base_model_name=base_model_name1,
+                                                    )
+                                else:
+                                    res_ls = infer_wmt(ckpt,
+                                                    task,
+                                                    dir_p+res_pth,
+                                                    test_set_take_num=500,
+                                                    mnt=64,
+                                                    base_model_name=base_model_name1,
+                                                    )
+                            else:
+                                # from collections import OrderedDict
+                                with open(dir_p+res_pth, 'r', encoding='utf8') as f:
+                                    res_ls = json.load(
+                                        f, object_pairs_hook=OrderedDict)
+
+                            scores = eval_wmt(res_ls)
                         else:
-                            # from collections import OrderedDict
-                            with open(dir_p+res_pth, 'r', encoding='utf8') as f:
-                                res_ls = json.load(
-                                    f, object_pairs_hook=OrderedDict)
+                            print("NOT FOUNDDDDDDDDDDDDDDDDDDDDDDD.")
 
-                        scores = eval_wmt(res_ls)
-                    else:
-                        print("NOT FOUNDDDDDDDDDDDDDDDDDDDDDDD.")
+                        another_dict=wrmk_dtct(
+                            res_ls,
+                            tokenizer,
+                            device="cuda:0",
+                            )
+                        print("=======================================")
+                        print("Detect-related Metric:")
+                        print(another_dict)
+                        print("Detect-related Metric.")
+                        print("=======================================")
 
-                    another_dict=wrmk_dtct(
-                        res_ls,
-                        tokenizer,
-                        device="cuda:0",
-                        )
-                    print("=======================================")
-                    print("Detect-related Metric:")
-                    print(another_dict)
-                    print("Detect-related Metric.")
-                    print("=======================================")
+                        for keyy in another_dict:
+                            if keyy =="z_score_at_T":
+                                continue
+                            scores[keyy]=another_dict[keyy]
 
-                    for keyy in another_dict:
-                        if keyy =="z_score_at_T":
-                            continue
-                        scores[keyy]=another_dict[keyy]
-                    
-                    print(task, ckpt)
-                    print(scores)
-                    res_dict[task+"-----"+res_pth] = scores
-                    score_ls=[
-                        scores["bleu"]["1"],
-                        scores["bleu"]["2"],
-                        scores["bleu"]["3"],
-                        scores["bleu"]["4"],
-                        scores["bertscore"]["p"],
-                        scores["bertscore"]["r"],
-                        scores["bertscore"]["f1"],
-                        scores["rouge-l"]["p"],
-                        scores["rouge-l"]["r"],
-                        scores["rouge-l"]["f1"],
-                        ]
-                    temp_scorels.append(score_ls)
+                        print(task, ckpt)
+                        print(scores)
+                        res_dict[task+"-----"+res_pth] = scores
+                        score_ls=[
+                            scores["bleu"]["1"],
+                            scores["bleu"]["2"],
+                            scores["bleu"]["3"],
+                            scores["bleu"]["4"],
+                            scores["bertscore"]["p"],
+                            scores["bertscore"]["r"],
+                            scores["bertscore"]["f1"],
+                            scores["rouge-l"]["p"],
+                            scores["rouge-l"]["r"],
+                            scores["rouge-l"]["f1"],
+                            ]
+                        temp_scorels.append(score_ls)
 
                 # obtain the mean value
                 # obtain the std value
