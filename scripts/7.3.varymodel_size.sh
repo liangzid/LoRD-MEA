0337deedba9a856def322b948d6453bdcdca7f13
liangzid <2273067585@qq.com>
2024-06-17 14:08:41 +0000

    add varying model size experiments.

---
+++ scripts/7.3.varymodel_size.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+######################################################################
+#7.3.VARYMODEL_SIZE ---
+
+# Experiments varying model scales.
+
+# Author: Zi Liang <zi1415926.liang@connect.polyu.hk>
+# Copyright © 2024, ZiLiang, all rights reserved.
+# Created: 17 June 2024
+######################################################################
+
+######################### Commentary ##################################
+##  
+######################################################################
+
+echo "HOME: ${HOME}"
+export python=${HOME}/anaconda3/envs/align/bin/python3
+export root_dir="${HOME}/alignmentExtraction/"
+export POD_save_dir="${root_dir}/SCALE_VARYING_CKPTS"
+export TRAIN_NUMS=(64)
+# export from_path_ls=("meta-llama/Meta-Llama-3-8B-Instruct")
+export from_path=$1
+export cudals=($2)
+# export cudals=("cpu")
+
+# export train_times=(2 3 4 5)
+export train_times=(1)
+export msl=140
+export task_ls=("cs-en")
+# export task_ls=("de-en")
+# export train_taskls=("vanilla" "LoRD-VI")
+# export train_taskls=("LoRD-VIII" "vanilla")
+export train_taskls=("LoRD-VIII")
+# export train_taskls=("vanilla")
+
+export is_black_box=1
+export use_lora=1
+
+# export epoch=3
+# export period=3
+
+export epoch=1
+export period=1
+
+export sub_set_num=1
+# export sub_stage_num=512
+export sub_stage_num=2048
+# export max_new_tokens=64
+export max_new_tokens=32
+export infer_batch_size=1
+export batch_size=1
+
+export beta=-1
+export temperature=-1
+
+export use_old_logits=1
+export use_vic_logits=1
+export use_kld=0
+export use_entropy=0
+
+# export tau1=0.85
+export tau1=0.80
+export tau2=0.85
+
+
+
+length=${#TRAIN_NUMS[@]}
+
+
+for (( i=0; i<$length; i++ )); do
+# for train_num in ${TRAIN_NUMS[*]}
+# do
+    
+    export CUDA_VISIBLE_DEVICES=${cudals[$i]}
+    export TORCH_USE_CUDA_DSA="1"
+    export train_num=${TRAIN_NUMS[$i]}
+
+    echo "train_num: ${train_num}\t device: ${cudals[$i]}"
+
+    for train_time in ${train_times[*]}
+    do
+	for task in ${task_ls[*]}
+	do
+	    for train_task in ${train_taskls[*]}
+	    do
+		echo "====================================================="
+		echo "+++++++train_num: ${train_num}+++++++"
+		echo "+++++++train_time: ${train_time}+++++++"
+		echo "+++++++task: ${task}+++++++"
+		echo "+++++++train_task: ${train_task}+++++++"
+		echo "====================================================="
+
+		export save_path="${POD_save_dir}text2sql${from_path}${task}${train_num}${train_time}${train_task}"
+
+		$python ${root_dir}lord_train.py\
+		    --use_lora=$use_lora \
+		    --from_path=$from_path \
+		    --is_black_box=$is_black_box \
+		    --sub_set_num=$sub_set_num \
+		    --sub_stage_num=$sub_stage_num\
+		    --infer_batch_size=$infer_batch_size\
+		    --tau1=$tau1 \
+		    --tau2=$tau2 \
+		    --task=$train_task \
+		    --device="cuda" \
+		    --epoch=$epoch \
+		    --period_num=$period \
+		    --acc_step=1 \
+		    --log_step=50 \
+		    --train_num=$train_num \
+		    --max_new_tokens=$max_new_tokens \
+		    --LR="3e-5" \
+		    --save_step=$sub_stage_num \
+		    --beta=$beta \
+		    --temperature=$temperature \
+		    --batch_size=$batch_size \
+		    --use_old_logits=$use_old_logits\
+		    --use_vic_logits=$use_vic_logits\
+		    --use_kld=$use_kld\
+		    --max_length=$msl \
+		    --dataset_task=$task \
+		    --save_path=$save_path
+
+		echo "DONE FOR ONE TRAIN NUMBERS...."
+
+	    done
+	done
+    done
+done
+
+
+
+
+
+
+
+
+
+
+
+echo "RUNNING 7.3.varymodel_size.sh DONE."
+# 7.3.varymodel_size.sh ends here
---
+++ scripts/meta_7.3.varysize.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+######################################################################
+#META_7.3.VARYSIZE --- 
+
+# Author: Zi Liang <zi1415926.liang@connect.polyu.hk>
+# Copyright © 2024, ZiLiang, all rights reserved.
+# Created: 17 June 2024
+######################################################################
+
+######################### Commentary ##################################
+##  
+######################################################################
+
+echo "HOME: ${HOME}"
+export python=${HOME}/anaconda3/envs/align/bin/python3
+export root_dir="${HOME}/alignmentExtraction/"
+
+# export model_ckpts=("facebook/opt-350m" "facebook/opt-1.3b" "facebook/opt-2.7b" "facebook/opt-6.7b" "facebook/opt-13b" )
+# export model_ckpts=("EleutherAI/pythia-410m" "EleutherAI/pythia-1.4b" "EleutherAI/pythia-2.8b" "EleutherAI/pythia-6.9b")
+export model_ckpts=("EleutherAI/pythia-410m" "EleutherAI/pythia-1.4b" "EleutherAI/pythia-2.8b" "EleutherAI/pythia-6.9b")
+# export model_ckpts=("EleutherAI/pythia-410m")
+
+
+export cudals=(0 1 2 3 4 5 6)
+
+length=${#model_ckpts[@]}
+
+for (( i=0; i<$length; i++ )); do
+    export model_ckpt=${model_ckpts[$i]}
+    export cudanum=${cudals[$i]}
+    export ckpt_part=$(echo "${model_ckpt}" | cut -d'/' -f2)
+    echo "cuda: $cudanum"
+
+# nohup bash ${root_dir}/scripts/7.3.varymodel_size.sh ${model_ckpt} ${cudanum} > "0617--TrainVaryingModelSize${ckpt_part}${cudanum}.log" &
+    nohup bash ${root_dir}/scripts/7.3.varymodel_size.sh ${model_ckpt} ${cudanum} > "0617--TrainVaryingModelSize${ckpt_part}${cudanum}.log" &
+done
+
+echo "sleep 3 hours Now."
+sleep 10800
+echo "sleep DONE."
+
+export model_ckpts=("facebook/opt-350m" "facebook/opt-1.3b" "facebook/opt-2.7b" "facebook/opt-6.7b" "facebook/opt-13b" )
+length=${#model_ckpts[@]}
+
+for (( i=0; i<$length; i++ )); do
+    export model_ckpt=${model_ckpts[$i]}
+    export cudanum=${cudals[$i]}
+    export ckpt_part=$(echo "${model_ckpt}" | cut -d'/' -f2)
+    echo "cuda: $cudanum"
+
+# nohup bash ${root_dir}/scripts/7.3.varymodel_size.sh ${model_ckpt} ${cudanum} > "0617--TrainVaryingModelSize${ckpt_part}${cudanum}.log" &
+    nohup bash ${root_dir}/scripts/7.3.varymodel_size.sh ${model_ckpt} ${cudanum} > "0617--TrainVaryingModelSize${ckpt_part}${cudanum}.log" &
+
+done
+
+
+# bash ${root_dir}/scripts/7.2.varytrainnum__wmt.sh 256 5 > "0615--trainvaryingtrainnum${trainnum}${cudanum}.log"
+
+# bash ${root_dir}/scripts/7.2.varytrainnum__wmt.sh 512 5 > "0615--trainvaryingtrainnum${trainnum}${cudanum}.log"
+
+# rm "${root_dir}wmt_0613_dataset_res/__NEW_VARYING_QUERYTIME_CKPTS__text2sqlcs-en2561LoRD-VIII___period2048_____cs-en_wmt_infer_resjson"
+# rm "${root_dir}wmt_0613_dataset_res/__NEW_VARYING_QUERYTIME_CKPTS__text2sqlcs-en5121LoRD-VIII___period2048_____cs-en_wmt_infer_resjson"
+# rm "${root_dir}wmt_0613_dataset_res/__NEW_VARYING_QUERYTIME_CKPTS__text2sqlde-en2561LoRD-VIII___period2048_____de-en_wmt_infer_resjson"
+# rm "${root_dir}wmt_0613_dataset_res/__NEW_VARYING_QUERYTIME_CKPTS__text2sqlde-en5121LoRD-VIII___period2048_____de-en_wmt_infer_resjson"
+
+# rm "${root_dir}wmt_0613_dataset_res/__NEW_VARYING_QUERYTIME_CKPTS__text2sqlde-en321vanilla___finally_____de-en_wmt_infer_resjson"
+
+echo "sleep 3 hours Now."
+sleep 10800
+echo "sleep DONE."
+
+$python "${root_dir}wmt_process.py"
+
+# rm "${root_dir}vary_query_num_overall_res_wmt16.json"
+# $python "${root_dir}eval_vary_trainNum.py"
+
+
+echo "RUNNING meta_7.3.varysize.sh DONE."
+# meta_7.3.varysize.sh ends here
