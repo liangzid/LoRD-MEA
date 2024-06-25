ff40b8f4b8bad4f20140795420e6d4ca1223cf7f
liangzid <2273067585@qq.com>
2024-06-25 21:20:05 +0800

    add general training experiments.

--- general_train/1.2.train_longtext.sh
+++ general_train/1.2.train_longtext.sh
@@ -15,7 +15,7 @@ echo "HOME: ${HOME}"
 export python=${HOME}/anaconda3/envs/align/bin/python3
 # export CUDA_VISIBLE_DEVICES="0,1,2,3"
 # export CUDA_VISIBLE_DEVICES="0,1,2"
-export CUDA_VISIBLE_DEVICES="0"
+export CUDA_VISIBLE_DEVICES="1"
 export root_dir="${HOME}/alignmentExtraction/"
 export POD_save_dir="${root_dir}/general_train/ckpts/boring_test/"
 export from_path="meta-llama/Meta-Llama-3-8B-Instruct"
@@ -51,6 +51,7 @@ export tau2=0.9
 export tau_delta=-0.1
 export save_step=3000
 export temperature=1.0
+export lambda1=0.5
 
 # export train_num=100
 
@@ -81,6 +82,7 @@ do
 		    --T=$temperature\
 		    --tau1=$tau1 \
 		    --tau2=$tau2 \
+		    --lambda1=$lambda1 \
 		    --task=$train_task \
 		    --device="cuda" \
 		    --epoch=$epoch \
@@ -102,41 +104,13 @@ do
 		    --save_path=$save_path
 		echo "DONE FOR ONE TRAIN NUMBERS...."
 
-# export qas=openbookqa,arc_easy,winogrande,hellaswag,arc_challenge,piqa,boolq
-# export qas=arc_challenge,hellaswag,winogrande,gsm8k
-export qas=arc_challenge,hellaswag,winogrande
-export eval=${HOME}/anaconda3/envs/align/bin/lm_eval
-export fmp="${save_path}___period6000/"
-
-echo "================================================================"
-echo "EVALUATION MODEL: pretrained: ${pmp} lora: ${fmp}"
-echo "EVALUATION TASKS: ${qas}"
-echo "================================================================"
-
-$eval --model hf \
-    --model_args pretrained=${pmp},parallelize=True,peft=${fmp}\
-    --tasks $qas\
-    --device cuda\
-    --batch_size auto:4
-
-
-export fmp="${save_path}___period3000/"
-
-echo "================================================================"
-echo "EVALUATION MODEL: pretrained: ${pmp} lora: ${fmp}"
-echo "EVALUATION TASKS: ${qas}"
-echo "================================================================"
-
-$eval --model hf \
-    --model_args pretrained=${pmp},parallelize=True,peft=${fmp}\
-    --tasks $qas\
-    --device cuda\
-    --batch_size auto:4
-
 	    done
 	done
     done
 done
+
+
+bash ${root_dir}/general_train/2.2.huggingface_llm_eval.sh
 
 echo "RUNNING 1.2.train_longtext.sh DONE."
 # 1.2.train_longtext.sh ends here
