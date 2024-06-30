bdae1bfec2c5037a080b6503f877e1831a650548
liangzid <2273067585@qq.com>
2024-06-30 20:14:48 +0800

    add eval varytriannum qa code.

--- eval_vary_trainNum.py
+++ eval_vary_trainNum.py
@@ -39,65 +39,68 @@ from qa_process import infer_qa, eval_qaacc
 def qa_curve_trainNums():
     method_ls = [
         "vanilla",
-        "kd",
-        "google/gemma-2b",
+        # "kd",
+        # "google/gemma-2b",
         # "lord",
         # "Complex-lord",
-        "LoRD-II",
-        "LoRD-IV",
+        "LoRD-VI",
+        # "LoRD-IV",
     ]
 
     marker = {
         "vanilla": "s",
-        "kd": "D",
+        # "kd": "D",
         # "Complex-lord": "o",
-        "LoRD-II": "v",
-        "LoRD-IV": "^",
-        "Complex-lord": "o",
-        "google/gemma-2b": "*",
+        # "LoRD-II": "v",
+        # "LoRD-IV": "^",
+        "LoRD-VI": "o",
+        # "google/gemma-2b": "*",
     }
 
     model_color_dict = {
         "vanilla": "#4a148c",
-        "kd": "#469de9",
-        "Complex-lord": "#eb3b5a",
-        "LoRD-II": "#eb3b5a",
-        "LoRD-IV": "#eb3b5a",
-        "google/gemma-2b": "#3867d6",
+        "LoRD-VI": "#eb3b5a",
+        # "kd": "#469de9",
+        # "Complex-lord": "#eb3b5a",
+        # "LoRD-II": "#eb3b5a",
+        # "LoRD-IV": "#eb3b5a",
+        # "google/gemma-2b": "#3867d6",
     }
 
     model_color_dict2 = {
         "vanilla": "#9c27b0",
-        "kd": "#98c8f3",
-        "Complex-lord": "#f78fb3",
-        "LoRD-II": "#f78fb3",
         "LoRD-IV": "#f78fb3",
-        "google/gemma-2b": "#778beb",
+        # "kd": "#98c8f3",
+        # "Complex-lord": "#f78fb3",
+        # "LoRD-II": "#f78fb3",
+        # "LoRD-IV": "#f78fb3",
+        # "google/gemma-2b": "#778beb",
     }
 
     taskls = [
         "piqa",
         "truthful_qa",
-        "allenai/ai2_arc",
+        # "allenai/ai2_arc",
     ]
 
     train_times = [
         "1",
-        "2",
-        "3",
+        # "2",
+        # "3",
     ]
 
-    train_nums = ["4", "8", "16", "32", "64", "100", "256", "512"]
+    train_nums = ["4", "8", "16", "32", "64", "128", "256", "512", "1024"]
 
     a = 0.4
     lw = 1.7
     model_line_style = {
         "vanilla": "-.",
-        "kd": "-.",
-        "Complex-lord": "-",
-        "LoRD-II": "-",
-        "LoRD-IV": "-",
-        "google/gemma-2b": "-",
+        "LoRD-VI": "-",
+        # "kd": "-.",
+        # "Complex-lord": "-",
+        # "LoRD-II": "-",
+        # "LoRD-IV": "-",
+        # "google/gemma-2b": "-",
     }
     font_size = 21
 
@@ -110,9 +113,9 @@ def qa_curve_trainNums():
     infer_save_pth = "./vary_query_num_overall_res_qa.json"
 
     if not os.path.exists(infer_save_pth):
-        dir_p = "./vary_train_num_qa_infers/"
+        dir_p = "./qa_0630_dataset_res/"
         # using existing results of the paths.
-        prefix = "./vArY_TrAiN_nUm_ckpts/varyTrainNum___"
+        prefix = "./varytrainnum_ckpts/qa"
         for task in taskls:
             results_dict[task] = {}
             acc_dict[task] = {}
@@ -141,21 +144,21 @@ def qa_curve_trainNums():
 
                         if m == "google/gemma-2b":
                             pth = m
-                        elif "LoRD" in m:
-                            pth = prefix + \
-                                f"{tn}{train_time}{task}{m}112164256___"
-                            pth += "period8"
                         else:
                             pth = prefix + \
-                                f"{tn}{train_time}{task}{m}332164256___"
+                                f"{task}{tn}{train_time}{m}___"
 
                             if m in [
                                 "vanilla",
                                 "kd",
                             ]:
-                                pth += "finally"
+                                pth += "finally/"
+                            elif tn=="1024":
+                                pth += "period1024/"
+                            elif tn=="2048":
+                                pth += "period2048/"
                             else:
-                                pth += "period2"
+                                pth += "period512/"
 
                         if m == "google/gemma-2b":
                             res_pth = pth + \
@@ -166,16 +169,12 @@ def qa_curve_trainNums():
 
                         print(f"Targeted found pth: {dir_p+res_pth}.")
                         if not os.path.exists(dir_p + res_pth):
-                            res_ls = infer_qa(
-                                pth,
-                                task,
-                                dir_p + res_pth,
-                                test_set_take_num=100,
-                                mnt=64,
-                            )
+                            print("ERORR..")
+                            return -10000
                         else:
                             # from collections import OrderedDict
-                            with open(dir_p + res_pth, "r", encoding="utf8") as f:
+                            with open(dir_p + res_pth,
+                                      "r", encoding="utf8") as f:
                                 res_ls = json.load(
                                     f, object_pairs_hook=OrderedDict)
 
@@ -206,7 +205,7 @@ def qa_curve_trainNums():
         res_dict[task]["Recall"] = r_dict[task]
         res_dict[task]["F1"] = f1_dict[task]
 
-    fig, axs = plt.subplots(3, 4, figsize=(20.8, 14.05))
+    fig, axs = plt.subplots(2, 4, figsize=(20.8, 9.04))
 
     for i, task in enumerate(list(res_dict.keys())):
         for j, metricName in enumerate(list(res_dict[task].keys())):
@@ -246,14 +245,14 @@ def qa_curve_trainNums():
                     color=model_color_dict[method],
                 )
 
-                axs[i][j].fill_between(
-                    xls,
-                    ymeanls - ystdls,
-                    ymeanls + ystdls,
-                    alpha=a,
-                    linewidth=0.0,
-                    color=model_color_dict2[method],
-                )
+                # axs[i][j].fill_between(
+                #     xls,
+                #     ymeanls - ystdls,
+                #     ymeanls + ystdls,
+                #     alpha=a,
+                #     linewidth=0.0,
+                #     color=model_color_dict2[method],
+                # )
 
                 axs[i][j].set_xlabel("# Training Samples", fontsize=font_size)
                 axs[i][j].set_ylabel(metricName, fontsize=font_size - 5)
@@ -301,19 +300,19 @@ def wmt_curve_trainNums():
         "vanilla": "s",
         # "kd": "D",
         # "google/gemma-2b": "*",
-        "LoRD-VIII": "o",
+        "LoRD-VI": "o",
     }
     model_color_dict = {
         "vanilla": "#4a148c",
         # "kd": "#469de9",
-        "LoRD-VIII": "#eb3b5a",
+        "LoRD-VI": "#eb3b5a",
         # "google/gemma-2b": "#3867d6",
     }
 
     model_color_dict2 = {
         "vanilla": "#9c27b0",
         # "kd": "#98c8f3",
-        "LoRD-VIII": "#f78fb3",
+        "LoRD-VI": "#f78fb3",
         # "google/gemma-2b": "#778beb",
     }
 
@@ -350,7 +349,7 @@ def wmt_curve_trainNums():
 
     if not os.path.exists(infer_save_pth):
         # dir_p = "./vary_train_num_WMT16_infers/"
-        dir_p = "./wmt_0629_dataset_res/"
+        dir_p = "./wmt_0613_dataset_res/"
         # using existing results of the paths.
         prefix = "./NEW_VARYING_QUERYTIME_CKPTS/text2sql"
         for task in taskls:
@@ -967,7 +966,7 @@ def glue_curve_trainNums():
 # running entry
 if __name__ == "__main__":
     # glue()
-    wmt_curve_trainNums()
+    # wmt_curve_trainNums()
     # code_curve_trainNums()
-    # qa_curve_trainNums()
+    qa_curve_trainNums()
     print("EVERYTHING DONE.")
